40e270121dd03b1c253603c2b56703fa9d58e2c1
cap-nascimento <andersonkuro@gmail.com>
2020-12-07 14:59:42 -0300

    changing project directory

--- anderson-portfolio/anderson-portfolio-web/WebContent/js/menu-toggle.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.toggle-nav').click(function(e) {
-        $(this).toggleClass('active');
-        $('.menu ul').fadeToggle(200);
-
-        e.preventDefault();
-    });
-});
